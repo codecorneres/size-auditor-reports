377031b84a1120cd1d7e1695e1101a197798ec2e
codecorneres <codecorneres@gmail.com>
2019-02-19 10:53:10 +0530

    remove file upload page

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import { Router, Route,Switch } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import './App.css';
 import { initializeIcons } from '@uifabric/icons';
-import Carousel from './components/Carousel';
+// import Carousel from './components/Carousel';
 import Landing from './components/Landing';
 import Module from './components/Module';
 import Asset from './components/Asset';
 import Pyramid from './components/Pyramid';
 import AssetsValue from './components/AssetsValue';
 import Feedback from './components/Feedback';
-import ExpressFileUpload from './components/ExpressFileUpload'
+// import ExpressFileUpload from './components/ExpressFileUpload'
 class App extends React.Component<any, any>  {
  public render(): JSX.Element {
   initializeIcons();
@@ -19,14 +19,14 @@ class App extends React.Component<any, any>  {
       <div className="App">
         <Router history={history}>
           <Switch>
-            <Route exact path='/carousel' component={Carousel} />
+            {/* <Route exact path='/carousel' component={Carousel} /> */}
             <Route exact path='/' component={Landing} />
             <Route exact path='/Module' component={Module} />
             <Route exact path='/Assets' component={Asset} />
             <Route exact path='/Pyramid' component={Pyramid} />
             <Route exact path='/Assets/:name' component={AssetsValue} />
             <Route exact path='/Feedback' component={Feedback} />
-            <Route exact path='/ExpressFileUpload' component={ExpressFileUpload} />
+            {/* <Route exact path='/ExpressFileUpload' component={ExpressFileUpload} /> */}
           </Switch>
         </Router>
       </div>
